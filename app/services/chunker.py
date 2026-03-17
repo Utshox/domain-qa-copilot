54badcb95452b6c41b72d0a0a6603a6ba94685bf
Utshox <32913328+Utshox@users.noreply.github.com>
2026-03-17 02:59:50 +0200

    Fix langchain import: use langchain_text_splitters package

--- app/services/chunker.py
+++ app/services/chunker.py
@@ -1,5 +1,5 @@
 from typing import List
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from app.core.config import settings
 
